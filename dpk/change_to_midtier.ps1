a1e4dbd0d57d73aebf7f32f8636e49acd508d078
Dan Iverson <dan@daniverson.com>
2018-01-29 10:47:25 -0600

    Adding puppet run to change_to_midtier

--- dpk/change_to_midtier.ps1
+++ dpk/change_to_midtier.ps1
@@ -9,4 +9,19 @@ function change_to_midtier() {
   Write-Host "[${computername}][Done] Change env_type to 'midtier'" -ForegroundColor Green
 }
 
+function deploy_oracle_client() {
+  Write-Host "[${computername}][Task] Run Puppet to Deploy Oracle Client"
+  Stop-Service Psft*
+  Set-Location $PUPPET_HOME\production\manifests
+  puppet apply .\site.pp --confdir=$PUPPET_HOME
+  $oracle_client_location = $(hiera oracle_client_location -c $PUPPET_HOME\hiera.yaml)
+  if (Test-Path $oracle_client_location) {
+    Write-Host "[${computername}][Done] Run Puppet to Deploy Oracle Client" -ForegroundColor Green
+  } else {
+    Write-Host "[${computername}][Error] Oracle Client did not deploy" -ForegroundColor Red
+  }
+  
+}
+
 . change_to_midtier
+. deploy_oracle_client
