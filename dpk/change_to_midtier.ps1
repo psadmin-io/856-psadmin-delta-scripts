b245e913858e312ac97cd5d6d1d7e1ce9d497a37
Dan Iverson <dan@daniverson.com>
2018-01-29 11:12:21 -0600

    Add Elapsed Time output

--- dpk/change_to_midtier.ps1
+++ dpk/change_to_midtier.ps1
@@ -1,21 +1,22 @@
 $computername = $env:computername
-$PUPPET_HOME="c:\psft\dpk\puppet"
-$START_LOCATION=$(Get-Location)
+$puppet_home="c:\psft\dpk\puppet"
+$start_location=$(Get-Location)
+$start_time = $(get-date)
 
 function change_to_midtier() {
   Write-Host "[${computername}][Task] Change env_type to 'midtier'"
-  (Get-Content "${PUPPET_HOME}\production\data\defaults.yaml").replace("env_type: fulltier", "env_type: midtier") | Set-Content "${PUPPET_HOME}\production\data\defaults.yaml"
-  (Get-Content "${PUPPET_HOME}\production\manifests\site.pp") -replace 'include.*', "include ::pt_role::pt_app_midtier" | Set-Content "${PUPPET_HOME}\production\manifests\site.pp"
-  Copy-Item -Path .\psft_customizations.yaml -Destination "${PUPPET_HOME}\production\data" 
+  (Get-Content "${puppet_home}\production\data\defaults.yaml").replace("env_type: fulltier", "env_type: midtier") | Set-Content "${puppet_home}\production\data\defaults.yaml"
+  (Get-Content "${puppet_home}\production\manifests\site.pp") -replace 'include.*', "include ::pt_role::pt_app_midtier" | Set-Content "${puppet_home}\production\manifests\site.pp"
+  Copy-Item -Path .\psft_customizations.yaml -Destination "${puppet_home}\production\data" 
   Write-Host "[${computername}][Done] Change env_type to 'midtier'" -ForegroundColor Green
 }
 
 function deploy_oracle_client() {
   Write-Host "[${computername}][Task] Run Puppet to Deploy Oracle Client"
   Stop-Service Psft* -WarningAction SilentlyContinue
-  Set-Location $PUPPET_HOME\production\manifests
-  puppet apply -e "include pt_profile::pt_tools_deployment" --confdir=$PUPPET_HOME  2>&1 | out-null
-  $oracle_client_location = $(hiera oracle_client_location -c $PUPPET_HOME\hiera.yaml)
+  Set-Location $puppet_home\production\manifests
+  puppet apply -e "include pt_profile::pt_tools_deployment" --confdir=$puppet_home  2>&1 | out-null
+  $oracle_client_location = $(hiera oracle_client_location -c $puppet_home\hiera.yaml)
   if (Test-Path $oracle_client_location) {
     Write-Host "[${computername}][Done] Run Puppet to Deploy Oracle Client" -ForegroundColor Green
   } else {
@@ -24,7 +25,14 @@ function deploy_oracle_client() {
   
 }
 
+function report_elapsed_time() {
+  $elapsedTime = $(get-date) - $start_time
+  $totalTime = "{0:HH:mm:ss}" -f ([datetime]$elapsedTime.Ticks)
+  Write-Host "[${computername}][Time] ${totalTime}"
+}
+
 . change_to_midtier
 . deploy_oracle_client
+. report_elapsed_time
 
-Set-Location $START_LOCATION
+Set-Location $start_location
