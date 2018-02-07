340b40b6e05b508d16b1b1ca86f368ef677866d3
Dan Iverson <dan@daniverson.com>
2018-02-06 21:20:28 -0600

    Adding ACM sample script

---
+++ acm/refresh_db.ps1
@@ -0,0 +1,22 @@
+[CmdletBinding()]
+Param(
+[String]$PS_HOME  = $env:PS_HOME,
+[String]$USER     = 'PS',
+[String]$PASS     = 'PS',
+[String]$DATABASE = 'HCMWIN',
+[String]$SERVER   = $(hostname)
+)
+
+$start_location = Get-Location
+Set-Location $PS_HOME\utility
+$env:PS_FILEDIR="${env:PS_CFG_HOME}\files"
+
+try {
+    ./psrunACM.bat $SERVER ORACLE $DATABASE $USER $PASS "${DATABASE}_CONFIG" EXEC
+    ./psrunACM.bat $SERVER ORACLE $DATABASE $USER $PASS "${DATABASE}_REFRESH" EXEC
+}
+catch {
+    Write-Host "Error running ACM for ${DATABASE} Refresh" -Foreground Red
+    Write-Host "Exit Code: $lastexitcode" -Foreground Yellow
+}
+Set-Location $start_location
