1d8f7c6ba0fe0d96da25cb6fc1de7616ece1923e
Dan Iverson <dan@daniverson.com>
2018-10-21 17:58:57 -0500

    Update ES Patch Number

--- es/install_es.ps1
+++ es/install_es.ps1
@@ -7,7 +7,8 @@ $MOSPass = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropSe
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 invoke-webrequest -uri https://github.com/MarisElsins/getMOSPatch/archive/V2.1.zip -outfile getMOSPatch.zip
 Expand-Archive getMOSPatch.zip -DestinationPath .\; copy-item .\getMOSPatch-2.1\getMOSPatch.jar c:\psft\
-java -jar c:\psft\getMOSPatch.jar patch=26938621 platform=233P MOSUser=dan@psadmin.io MOSPass=$MOSPass download=all
+java -jar c:\psft\getMOSPatch.jar patch=28615231 platform=233P MOSUser=dan@psadmin.io MOSPass=$MOSPass download=all
 expand-archive .\ELASTICSEARCH*04.zip -DestinationPath .\es004
 set-location .\es004\setup
 .\psft-dpk-setup.bat --install --install_base_dir=c:\psft\elastic
+ 
